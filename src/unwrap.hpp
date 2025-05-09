dac3e0230a30aeef5b050f9afc906c3f817caab2
mojyack <mojyack@gmail.com>
2025-05-09 11:49:27 +0900

    unwrap: add unwrap_a

--- src/unwrap.hpp
+++ src/unwrap.hpp
@@ -11,9 +11,16 @@
     ensure_v(var##_o, __VA_ARGS__);                      \
     const auto& var = *var##_o;
 
+#define generic_unwrap_a(ensure_v, const, var, opt, ...) \
+    const auto var##_o = opt;                            \
+    ensure_a(var##_o, __VA_ARGS__);                      \
+    const auto& var = *var##_o;
+
 #define unwrap(var, opt, ...)          generic_unwrap(ensure, const, var, opt, __VA_ARGS__)
 #define unwrap_mut(var, opt, ...)      generic_unwrap(ensure, /*const*/, var, opt, __VA_ARGS__)
 #define unwrap_v(var, opt, ...)        generic_unwrap_v(ensure_v, const, var, opt, __VA_ARGS__)
 #define unwrap_v_mut(var, opt, ...)    generic_unwrap_v(ensure_v, /*const*/, var, opt, __VA_ARGS__)
+#define unwrap_a(var, opt, ...)        generic_unwrap_a(ensure_v, const, var, opt, __VA_ARGS__)
+#define unwrap_a_mut(var, opt, ...)    generic_unwrap_a(ensure_v, /*const*/, var, opt, __VA_ARGS__)
 #define co_unwrap_v(var, opt, ...)     generic_unwrap_v(co_ensure_v, const, var, opt, __VA_ARGS__)
 #define co_unwrap_v_mut(var, opt, ...) generic_unwrap_v(co_ensure_v, /*const*/, var, opt, __VA_ARGS__)
